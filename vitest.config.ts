d16af187cc954b03f1691df597537fcf2173cfb9
Kyle McNally <kyle@kmcnally.net>
2025-05-08 08:30:47 -0400

    Fix coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
+      include: ["src/**/*"],
       reporter: ["json", "json-summary", "text"],
       reportOnFailure: true,
     },
